e8b12dc4ab433d47e2c94afcb960244354dd9d7a
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2019-08-29 16:02:11 +0530

    error

---
+++ src/displays.java
@@ -0,0 +1,7 @@
+public class displays {
+    public static void main(String[] args) {
+        int x = 123456;
+        double f = 100.12;
+        System.out.print("the float value= " + f);
+    }
+}
